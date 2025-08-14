6d5bfe7f312a2a3643547ad7cad4b5f8dcd1be1a
Richard Gill <richard@rgill.co.uk>
2025-08-14 10:49:11 +0100

    Removed incorrect comment from thumb cluster section

--- keyboards/cyboard/imprint/imprint_number_row_full_bottom_row/keymaps/default/keymap.c
+++ keyboards/cyboard/imprint/imprint_number_row_full_bottom_row/keymaps/default/keymap.c
@@ -70,7 +70,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
         /* Top left thumb cluster  */
-        /* Bottom right thumb cluster row */
         KC_NO, KC_NO, KC_NO,
         /* Top right thumb cluster  */
         KC_NO, KC_NO, KC_NO,
